555bb80814af1af73563aa8cfe44b2f7dd562456
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-03-12 14:05:52 +0530

    Funcitons functionality added

--- egg.js
+++ egg.js
@@ -214,3 +214,42 @@ run("do(define(total, 0),",
     "         do(define(total, +(total, count)),",
     "             define(count, +(count, 1)))),",
     "   print(total))");
+
+specialForms.fun = (args, scope) => {
+  if (!args.length) {
+    throw new SyntaxError("Functions need a body");
+  }
+  let body = args[args.length - 1];
+  let params = args.slice(0, args.length - 1).map(expr => {
+    if (expr.type != "word") {
+      throw new SyntaxError("Parameter names must be words");
+    }
+    return expr.name;
+  });
+
+  return function() {
+    if (arguments.length != params.length) {
+      throw new TypeError("Wrong number of arguments");
+    }
+    let localScope = Object.create(scope);
+    for (let i = 0; i < arguments.length; i++) {
+      localScope[params[i]] = arguments[i];
+    }
+    return evaluate(body, localScope);
+  };
+};
+
+run(`
+do(define(plusOne, fun(a, +(a, 1))),
+   print(plusOne(10)))
+`);
+// → 11
+
+run(`
+do(define(pow, fun(base, exp,
+     if(==(exp, 0),
+        1,
+        *(base, pow(base, -(exp, 1)))))),
+   print(pow(2, 10)))
+`);
+// → 1024
